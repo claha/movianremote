742ce51ff51d0590b5e947ad224649434cacc09a
Claes Hallstrom <hallstrom.claes@gmail.com>
2015-01-22 10:52:54 +0100

    Added CircularPagerAdapter class

--- app/src/main/java/com/claha/showtimeremote/BaseFragment.java
+++ app/src/main/java/com/claha/showtimeremote/BaseFragment.java
@@ -1,9 +1,9 @@
 package com.claha.showtimeremote;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -32,93 +32,39 @@ public abstract class BaseFragment extends Fragment {
 
         if (profiles.isEmpty()) {
             profiles.add(showtimeSettings.getIPAddress());
-        } else if (profiles.size() > 1) {
-            profiles.add(0, profiles.get(profiles.size() - 1));
-            profiles.add(profiles.get(0));
         }
 
-        ProfileAdapter adapter = new ProfileAdapter(profiles);
+        ProfileAdapter adapter = new ProfileAdapter(viewPager, profiles);
         viewPager.setAdapter(adapter);
 
         int index = showtimeSettings.loadProfiles().indexOf(showtimeSettings.getCurrentProfile()) + 1;
         viewPager.setCurrentItem(index);
 
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            private int currentPage = 0;
-
-            @Override
-            public void onPageScrolled(int i, float v, int i2) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                currentPage = position;
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                int pageCount = profiles.size();
-
-                if (state == ViewPager.SCROLL_STATE_IDLE && pageCount > 1) {
-
-                    if (currentPage == 0) {
-                        viewPager.setCurrentItem(pageCount - 2, false);
-                    } else if (currentPage == pageCount - 1) {
-                        viewPager.setCurrentItem(1, false);
-                    }
-                    showtimeSettings.setCurrentProfile(showtimeSettings.loadProfiles().get(currentPage - 1));
-                }
-            }
-        });
     }
 
     protected abstract int getLayoutResource();
 
-    public class ProfileAdapter extends PagerAdapter {
+    private class ProfileAdapter extends CircularPagerAdapter<String> {
 
-        final List<String> profiles;
-
-        public ProfileAdapter(List<String> profiles) {
-            this.profiles = profiles;
+        public ProfileAdapter(ViewPager viewPager, List<String> data) {
+            super(viewPager, data);
         }
 
         @Override
-        public int getCount() {
-            return profiles.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return view == o;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-
-            int pageCount = profiles.size();
-            if (pageCount > 1) {
-                if (position == 0) {
-                    position = pageCount - 2;
-                } else if (position == pageCount - 1) {
-                    position = 1;
-                }
-            }
-
-            TextView view = new TextView(getActivity());
-            view.setText(profiles.get(position));
+        protected View instantiateView(Context context, String item) {
+            TextView view = new TextView(context);
+            view.setText(item);
             view.setGravity(Gravity.CENTER);
             view.setTextColor(0xFFFFFFFF);
             view.setTypeface(Typeface.DEFAULT_BOLD);
-
-            container.addView(view);
-
             return view;
         }
 
         @Override
-        public void destroyItem(ViewGroup container, int position, Object view) {
-            container.removeView((View) view);
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            position = getOriginalPosition(position);
+            showtimeSettings.setCurrentProfile(showtimeSettings.loadProfiles().get(position));
         }
     }
 }
